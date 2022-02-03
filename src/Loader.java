0284b1ad71ff8c84dc21bf6cfdc0783916374e20
KirylDz <dekv78@gmail.com>
2022-02-03 21:37:04 +0300

    i have fixed bug

--- src/Loader.java
+++ src/Loader.java
@@ -6,6 +6,6 @@ public class Loader {
         System.out.printf("Вес кошки N1: %.2f %n", dog1.getWeight());
         dog1.feed(200.0);
         dog1.woof();
-        System.out.printf("Вес кошки N1 после кормежки: %.2f %n", dog1.getWeight());
+        System.out.printf("Вес БОЛЬШОЙ  кошки N1 после кормежки: %.2f %n", dog1.getWeight());
     }
 }
